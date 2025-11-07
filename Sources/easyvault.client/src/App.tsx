f488864c4167954add0ce51d194a79a4a4e959a0
Vadim Belov <bvdcode@users.noreply.github.com>
2025-11-06 22:57:52 -0800

    Add GitHub link button to the App component

--- Sources/easyvault.client/src/App.tsx
+++ Sources/easyvault.client/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import { Box, CssBaseline } from "@mui/material";
+import { Box, CssBaseline, Fab } from "@mui/material";
 import { LoginPage, VaultPage } from "./pages";
 import "react-toastify/dist/ReactToastify.css";
 import { ConfirmProvider } from "material-ui-confirm";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GitHub } from "@mui/icons-material";
 
 function App() {
   return (
@@ -18,6 +19,16 @@ function App() {
             </Routes>
           </BrowserRouter>
           <CssBaseline enableColorScheme={true} />
+          <Fab
+            color="primary"
+            aria-label="add"
+            sx={{ position: "fixed", bottom: 16, right: 16 }}
+            href="https://github.com/bvdcode/EasyVault"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHub />
+          </Fab>
         </ConfirmProvider>
       </ThemeProvider>
     </Box>
